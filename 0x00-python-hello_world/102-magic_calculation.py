2fd11e336d9c5c99f9d807314421f226a4b49375
EDEN <issamghled@gmail.com>
2023-11-27 13:25:45 +0100

    4

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,15 +1,5 @@
 #!/usr/bin/python3
-import dis
-
 def magic_calculation(a, b):
-    return 98 + (a ** b)
-
-# Verify that the function behaves the same as the bytecode
-def test_magic_calculation():
-    a = 2
-    b = 5
-    bytecode_result = eval(compile("98 + (a ** b)", '<string>', 'eval'))
-    function_result = magic_calculation(a, b)
-    assert bytecode_result == function_result
-
-test_magic_calculation()
+    result = 98
+    result += a ** b
+    return result
