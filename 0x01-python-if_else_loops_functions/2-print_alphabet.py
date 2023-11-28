02cc8f69424cdb96be8f4c4510c9e2101c509363
EDEN <issamghled@gmail.com>
2023-11-28 10:17:50 +0100

    4

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,2 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if i not in [101, 113]:  # ASCII values for 'e' and 'q'
-        print("{}".format(chr(i)), end="")
+print("".join(chr(i) for i in range(97, 123) if i not in [101, 113]), end="")
