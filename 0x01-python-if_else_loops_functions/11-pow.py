4b833c17b0e9a5fde0862f8907086477e3da35c1
EDEN <issamghled@gmail.com>
2023-11-28 10:39:27 +0100

    4

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,12 +1,16 @@
-#!/usr/bin/python3
 def pow(a, b):
     result = 1
 
+    # If the exponent is negative, compute the reciprocal
     if b < 0:
         a = 1 / a
         b = -b
 
+    # Multiply 'a' by itself 'b' times
     for _ in range(b):
         result *= a
 
-    return result
+    return round(result, 2)
+
+# Test cases
+print(pow(0.1, -2))
