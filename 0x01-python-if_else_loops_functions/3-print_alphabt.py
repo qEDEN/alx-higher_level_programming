1324cbff81e0b0b0671a83c5c4ed39d8726cedd2
EDEN <issamghled@gmail.com>
2023-11-28 10:18:50 +0100

    4

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,2 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if i == 101 or i == 113:
-        continue
-    print("{}".format(chr(i)), end='')
+print("".join(chr(i) for i in range(97, 123) if i not in [101, 113]), end="")
