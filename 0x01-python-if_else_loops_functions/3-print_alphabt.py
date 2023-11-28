c196d07d159ca7af4e2f053493cf28fb888e904e
EDEN <issamghled@gmail.com>
2023-11-28 10:19:29 +0100

    4

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
-print("".join(chr(i) for i in range(97, 123) if i not in [101, 113]), end="")
+for i in range(97, 123):
+    if i not in [101, 113]:  # ASCII values for 'e' and 'q'
+        print("{}".format(chr(i)), end="")
