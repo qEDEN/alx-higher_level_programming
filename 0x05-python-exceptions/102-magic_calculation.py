f3eda59f372f4adc200f11afa196508ebd20aea8
EDEN <issamghled@gmail.com>
2023-12-18 15:01:11 +0100

    4

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
     result = 0
+
     for i in range(1, 3):
         try:
             if i > a:
@@ -10,4 +11,5 @@ def magic_calculation(a, b):
         except:
             result = b + a
             break
+
     return result
