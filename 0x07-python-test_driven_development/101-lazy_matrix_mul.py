8a65168572c396d1bad20019c59847a11439ac42
EDEN <issamghled@gmail.com>
2024-01-10 20:15:26 +0100

    4

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,10 +1,35 @@
 #!/usr/bin/python3
-"""
-Defines lazy_matrix function
-"""
-import numpy
-
+import numpy as np
 
 def lazy_matrix_mul(m_a, m_b):
-    """ calculates the matrix multiplication of two matrices"""
-    return numpy.matmul(m_a, m_b)
+    """
+    Multiplies two matrices using NumPy.
+
+    Args:
+        m_a (list): The first matrix.
+        m_b (list): The second matrix.
+
+    Returns:
+        numpy.ndarray: The resulting matrix.
+
+    Raises:
+        ValueError: If the matrices cannot be multiplied.
+
+    Example:
+    >>> lazy_matrix_mul([[1, 2], [3, 4]], [[1, 2], [3, 4]])
+    array([[ 7, 10],
+           [15, 22]])
+
+    >>> lazy_matrix_mul([[1, 2]], [[3, 4], [5, 6]])
+    array([[13, 16]])
+    """
+    try:
+        result = np.dot(np.array(m_a), np.array(m_b))
+        return result.tolist()
+    except ValueError as e:
+        raise ValueError("Matrices cannot be multiplied") from e
+
+if __name__ == "__main__":
+    # Test cases
+    print(lazy_matrix_mul([[1, 2], [3, 4]], [[1, 2], [3, 4]]))
+    print(lazy_matrix_mul([[1, 2]], [[3, 4], [5, 6]]))
